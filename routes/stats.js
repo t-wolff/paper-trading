b258a94ab13bd691cc76ebdb806b852d2a6845b4
t-wolff <wtehilla@yahoo.com>
2024-01-15 01:06:07 +0200

    backed user leaderbord pnl done

--- routes/stats.js
+++ routes/stats.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { calcStats } = require('../controllers/stats');
+const { calcStats, userStats } = require('../controllers/stats');
 const router = express.Router();
 
+router.get('/', userStats);
 router.get('/:userID', calcStats);
 
 module.exports = router;
