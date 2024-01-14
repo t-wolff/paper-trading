4636f90e4307e96c8c0ec2cf3d13f5364955cc07
t-wolff <wtehilla@yahoo.com>
2024-01-14 09:33:55 +0200

    can change qty

--- client/src/pages/trade/Trade.jsx
+++ client/src/pages/trade/Trade.jsx
@@ -8,16 +8,36 @@ import { useDispatch, useSelector } from 'react-redux';
 import * as actionTrade from '../../redux/Trade/tradeSlice';
 import './Trade.css';
 import StyledButton from '../../components/styledButton/StyledButton';
+import Input from '../../components/input/Input';
 
 const Trade = () => {
 	const dispatch = useDispatch();
 	const [price, setPrice] = useState();
-    const [product, setProduct] = useState('btc');
-    const [quantity, setQuantity] = useState(0.001);
+	const [product, setProduct] = useState('btc');
+	const [quantity, setQuantity] = useState(0.001);
 	const userContent = useSelector((state) => state.auth.userContent);
 	const userId = userContent.userID;
 
 	useEffect(() => {
+		// Get all cookies as a string
+		const allCookiesString = document.cookie;
+
+		// Parse the string to get individual cookies
+		const cookiesArray = allCookiesString.split(';');
+
+		// Create an object to store key-value pairs of cookies
+		const cookies = {};
+
+		// Iterate through the array and populate the cookies object
+		cookiesArray.forEach((cookie) => {
+			const [name, value] = cookie.trim().split('=');
+			cookies[name] = value;
+		});
+
+		// Now, you can access individual cookies using the cookies object
+		const token = cookies.token;
+		console.log('Token from cookies:', token);
+
 		const connectToWebSocket = async () => {
 			try {
 				openWebSocket(
@@ -28,6 +48,10 @@ const Trade = () => {
 			}
 		};
 
+		// hardcoded for now need to change to dynamic rpoduct
+		setProduct('btc');
+		//
+
 		const showClosePrice = (msg) => {
 			const integerValue = parseFloat(msg.k.c, 10);
 			setPrice(integerValue);
@@ -37,19 +61,30 @@ const Trade = () => {
 		setMessageHandler(showClosePrice);
 	}, []);
 
-    const handleBuy = async () => {
-			dispatch(actionTrade.createTrade(product,'BUY',quantity,userId));
-		}
+	const handleBuy = async () => {
+		dispatch(actionTrade.createTrade(product, 'BUY', quantity, userId));
+	};
 
-    const handleSell = async () => {
-       dispatch(actionTrade.createTrade(product, 'SELL', quantity, userId));
-    }
+	const handleSell = async () => {
+		dispatch(actionTrade.createTrade(product, 'SELL', quantity, userId));
+	};
+
+	const handleChangeInputs = (e) => {
+		setQuantity(e.target.value);
+	};
 
 	return (
 		<div>
 			<h2>Trading Zone</h2>
-            <h2>{product}</h2>
+			<h2>{product}</h2>
 			<h2>{price}</h2>
+			<Input
+				name="quantity"
+				label="QTY"
+				type="string"
+				handleChange={(e) => handleChangeInputs(e)}
+				value={quantity}
+			/>
 			<StyledButton color={'green'} onclick={(e) => handleBuy(e)}>
 				BUY
 			</StyledButton>
