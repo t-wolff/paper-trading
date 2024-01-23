86d25a9e5baa72c10ae77b33467b8daf98434ff3
t-wolff <wtehilla@yahoo.com>
2024-01-23 14:41:40 +0200

    changes env to mode front

--- client/src/components/profilePic/ProfilePic.jsx
+++ client/src/components/profilePic/ProfilePic.jsx
@@ -4,6 +4,12 @@ import * as actionAuth from '../../redux/Auth/authSlice';
 import FileUpload from '../fileUpload/FileUpload';
 import './ProfilePic.css';
 
+const BASE_URL =
+	import.meta.env.VITE_MODE === 'production'
+		? import.meta.env.VITE_BASE_URL_PROD_PIC
+		: import.meta.env.VITE_BASE_URL_PIC;
+
+
 const ProfilePic = () => {
 	const dispatch = useDispatch();
 	const userContent = useSelector((state) => state.auth.userContent);
@@ -24,7 +30,7 @@ const ProfilePic = () => {
 	};
 
 	const imageUrl = userContent?.profilePic
-		? encodeURI(`http://localhost:5000/${
+		? encodeURI(`${BASE_URL}${
 				userContent?.profilePic}`)
 		: null;
 
